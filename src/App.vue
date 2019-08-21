44a54b2bebe452d8dc02347e1cdb3d61c83544d6
jeffholst <jeffholst@gmail.com>
2019-08-20 20:51:16 -0500

    Blast off!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <p class="text-center display-4" >{{timerDisplay}}</p>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
+  name: 'App',
+  methods: {
+    mouseClicked() {
+      if (this.timerStarted) {
+        this.timerStarted = false;
+        this.stopTimer();
+      } else {
+        this.timerStarted = true;
+        this.startTimer();
+      }
+    },
+    startTimer() {
+      this.timer = setInterval(this.updateTimer, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+    },
+    updateTimer() {
+      this.totalSeconds += 1;
+      this.timerDisplay = `${this.GetHours()}:${this.GetMinutes()}:${this.GetSeconds()}`;
+    },
+    GetSeconds() {
+      /*
+        return seconds portion 00:00:ss
+      */
+      const sec = this.totalSeconds % 60;
+      return sec >= 10 ? sec : `0${sec}`;
+    },
+    GetMinutes() {
+      /*
+        return minutes portion 00:mm:00
+      */
+      const min = Math.floor((this.totalSeconds / 60) % 60);
+      return min >= 10 ? min : `0${min}`;
+    },
+    GetHours() {
+      /*
+        return hours portion hh:00:00
+      */
+      const hrs = Math.floor(this.totalSeconds / 60 / 60);
+      return hrs >= 10 ? hrs : `0${hrs}`;
+    },
   },
+  created: function () {
+    const self = this;
+    document.body.addEventListener('click', () => { self.mouseClicked(); }, true);
+  },
+  data: () => ({
+    timer: 0,
+    totalSeconds: 0,
+    timerDisplay: '00:00:00',
+    timerStarted: false,
+  }),
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
